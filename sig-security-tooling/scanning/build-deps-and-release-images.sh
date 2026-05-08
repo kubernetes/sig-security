55ba18fa78de98c8eaa1810c2592e22f269390b4
Eric Smalling <github@ericsmalling.com>
2026-05-08 16:02:17 +0000

    Reverting snyk cli version pinning as they have resolved go scanning bugs

--- sig-security-tooling/scanning/build-deps-and-release-images.sh
+++ sig-security-tooling/scanning/build-deps-and-release-images.sh
@@ -15,8 +15,7 @@
 
 set -euo pipefail
 apt update && apt -y install jq
-# Hard-coding v1.1296.2 temporarily due to defect in newer versions per Snyk support case number 00123453
-wget -q -O /usr/local/bin/snyk https://static.snyk.io/cli/v1.1296.2/snyk-linux && chmod +x /usr/local/bin/snyk
+wget -q -O /usr/local/bin/snyk https://static.snyk.io/cli/latest/snyk-linux && chmod +x /usr/local/bin/snyk
 mkdir -p "${ARTIFACTS}"
 if [ -z "${SNYK_TOKEN}" ]; then
     echo "SNYK_TOKEN env var is not set, required for snyk scan"
